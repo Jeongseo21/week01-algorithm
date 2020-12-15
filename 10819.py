b383e97866db180f3c5186add8d0def154782b09
Jeongseo Han <jeongseo21@gmail.com>
2020-12-16 08:53:15 +0900

    .

--- 10819.py
+++ 10819.py
@@ -13,3 +13,23 @@ for array in arrays:
         result += abs(array[i]-array[i-1])
     answer.append(result)
 print(max(answer))
+
+'''
+#순열
+import itertools
+
+arr = ['A', 'B', 'C']
+nPr = itertools.permutations(arr, 2) # n개 원소 중에 2개를 뽑아 순서를 정해 나열하기
+print(list(nPr))
+
+결과 : [('A', 'B'), ('A', 'C'), ('B', 'A'), ('B', 'C'), ('C', 'A'), ('C', 'B')]
+
+#조합
+import itertools
+
+arr = ['A', 'B', 'C']
+nCr = itertools.combinations(arr, 2) # n개의 원소 중에 2개를 뽑기
+print(list(nCr))
+
+결과 : [('A', 'B'), ('A', 'C'), ('B', 'C')]
+'''
